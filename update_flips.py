0f87b133396c272a882fa08c84c45a6ec961327a
nfgems <73572237+nfgems@users.noreply.github.com>
2025-03-31 16:20:51 -0400

    Update update_flips.py

--- update_flips.py
+++ update_flips.py
@@ -13,7 +13,38 @@ secret_key = os.getenv("ALPACA_SECRET_KEY")
 
 client = StockHistoricalDataClient(api_key, secret_key)
 
-SP500 = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "BRK.B", "UNH", "V", "XOM", "JNJ", "JPM", "LLY", "PG", "AVGO", "MA", "HD", "CVX", "MRK"]  # Sample only
+SP500 = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "BRK.B", "UNH", "V",
+    "XOM", "JNJ", "JPM", "LLY", "PG", "AVGO", "MA", "HD", "CVX", "MRK",
+    "ABBV", "COST", "PEP", "KO", "BAC", "ADBE", "CRM", "WMT", "PFE", "NFLX",
+    "TMO", "ACN", "ABT", "LIN", "ORCL", "DIS", "NKE", "CSCO", "MCD", "CMCSA",
+    "DHR", "AMD", "INTC", "QCOM", "TXN", "NEE", "HON", "AMGN", "PM", "UPS",
+    "UNP", "MS", "IBM", "BMY", "GS", "LOW", "RTX", "GE", "SPGI", "CAT",
+    "ISRG", "AMT", "SBUX", "MDT", "INTU", "VRTX", "T", "BLK", "NOW", "CI",
+    "CVS", "ADI", "DE", "ZTS", "SYK", "MDLZ", "AXP", "C", "ELV", "MO",
+    "REGN", "GILD", "MU", "PLD", "BKNG", "LRCX", "PANW", "ADP", "TJX", "SCHW",
+    "CL", "FISV", "MMC", "CB", "HCA", "LMT", "TGT", "APD", "DUK", "F",
+    "WM", "PGR", "SO", "EOG", "ETN", "NSC", "SHW", "BDX", "EMR", "ITW",
+    "FDX", "COF", "PSX", "HUM", "CME", "NOC", "EW", "MAR", "AON", "OXY",
+    "ADSK", "MET", "ROST", "MNST", "STZ", "AIG", "KMB", "MCO", "AFL", "GIS",
+    "SLB", "IDXX", "AJG", "VLO", "PRU", "ORLY", "KMI", "EXC", "PEG", "PSA",
+    "DVN", "DLR", "CTSH", "HSY", "PCAR", "MSCI", "BKR", "TRV", "ALL", "HPQ",
+    "HLT", "SRE", "SPG", "KHC", "ED", "YUM", "AME", "AZO", "MTD", "PXD",
+    "TEL", "MCK", "CMG", "KR", "WELL", "IQV", "WMB", "AEP", "FAST", "TT",
+    "DLTR", "EFX", "VRSK", "AMP", "CHD", "WST", "CDNS", "WTW", "CTVA", "ANET",
+    "FTNT", "BIIB", "ECL", "VFC", "PAYX", "TSCO", "HES", "RSG", "WBD", "MLM",
+    "OTIS", "KEYS", "PPL", "FANG", "CNC", "OKE", "AVB", "FLT", "NEM", "D",
+    "HAL", "SBAC", "ODFL", "CMS", "ABC", "PH", "INVH", "IR", "TTWO", "VTR",
+    "RMD", "CBRE", "XYL", "DPZ", "TSN", "EXR", "CARR", "BR", "CEG", "DRI",
+    "GWW", "BALL", "LDOS", "DG", "IFF", "PPG", "MAS", "CFG", "HBAN", "ZBRA",
+    "NUE", "LUV", "ALB", "LHX", "CTRA", "CLX", "DOV", "AEE", "MKC", "COO",
+    "SWKS", "AKAM", "BAX", "RF", "STE", "CHRW", "HII", "NDAQ", "JBHT",
+    "STT", "ESS", "CAG", "WAT", "JKHY", "IPG", "TECH", "PKI", "MOS", "GEN",
+    "NRG", "HOLX", "MKTX", "ZION", "FMC", "NDSN", "BIO", "VTRS", "TER",
+    "WRB", "BEN", "GRMN", "BBY", "DHI", "TXT", "PFG", "TPR", "HSIC",
+    "LNT", "CPB", "ROL", "NI", "K", "IRM", "PNW", "AIZ", "HWM", "WHR"
+]
+
 
 seen_flips = {}
 
